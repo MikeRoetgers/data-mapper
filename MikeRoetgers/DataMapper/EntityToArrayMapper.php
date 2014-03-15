8d44492d49e1caff92c38c32c161baf968dc695a
Mike Rötgers <mike@roetgers.org>
2014-03-15 16:01:38 +0100

    bugfix method names

--- MikeRoetgers/DataMapper/EntityToArrayMapper.php
+++ MikeRoetgers/DataMapper/EntityToArrayMapper.php
@@ -5,5 +5,5 @@ namespace MikeRoetgers\DataMapper;
 interface EntityToArrayMapper
 {
     public function mapEntityToArray($entity, array $mappings = array());
-    public function massMapArrayToEntity(array $entities, array $mappings = array());
+    public function massMapEntityToArray(array $entities, array $mappings = array());
 }
--- MikeRoetgers/DataMapper/EntityToJsonMapper.php
+++ MikeRoetgers/DataMapper/EntityToJsonMapper.php
@@ -5,5 +5,5 @@ namespace MikeRoetgers\DataMapper;
 interface EntityToJsonMapper
 {
     public function mapEntityToJson($entity, array $mappings = array());
-    public function massMapJsonToEntity(array $entities, array $mappings = array());
+    public function massMapEntityToJson(array $entities, array $mappings = array());
 }
